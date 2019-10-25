cd8a875f8a47cd0f34ea2788b6ae345d5a091f13
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-10-25 09:59:48 -0600

    User and Event models created

---
+++ models/events/events.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+	name: {type: String, required: true}, 
+	location: {type: String, required: true},
+	date: {type: Date, required: true},
+	time: {type: Date, required: true},
+	eventPic: String,
+	description: String,
+	attendees: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}];
+});
+
+
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
---
+++ models/users/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+	name: {type: String, required: true},
+	displayName: {type: String, required: true},
+	profilePic: type: String,
+	location: {type: String, required: true},
+	email: {type: String, required: true},
+	password: {type: String, required: true},
+	isOrganizer: {type: Boolean, required: true},
+	events: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Event'
+	}]
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
